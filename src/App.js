79859ee0a44c24499fc29d58d01b1f81ed3a001f
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2020-11-01 21:13:52 +0500

    action payload completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React,{useState} from 'react';
+import './style.css'
+import { useDispatch, useSelector } from 'react-redux';
+import { buyCake } from './components/redux/action'
 
 
 function App() {
 
+  const [number, setNumber] = useState(1)
+  const numOfCakes = useSelector(state => state.numOfCakes)
+  const dispatch = useDispatch()
 
   return (
-    <div>
-      <h1>hello world</h1>
+    <div className='App'>
+      <h1>Number of Cakes - {numOfCakes}</h1>
+      <input type="text" value={number} onChange={(e) => setNumber(e.target.value)} />
+      <button onClick={() => dispatch(buyCake(number))}>BUY CAKE</button>
     </div>
   );
 }
--- src/components/store/fetchUser.js
+++
@@ -1,8 +0,0 @@
-const fetchUser = async () => {
-    const res = await fetch ('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-    const userID = data.map((val)=> val.id)
-    return userID;
-}
-
-export default fetchUser;
